8a0297fbf04f4ab4c2bb27bb3c759cf7d6ad761b
Ali Mammadli <ali.mammadli@novum.tech>
2022-01-22 16:19:00 +0400

    Unnecessary comments removed

--- assets/scripts/scroll.js
+++ assets/scripts/scroll.js
@@ -29,10 +29,3 @@ function scroll(evt) {
         }
     }
 }
-
-// var item = document.getElementById("scroll");
-
-// window.addEventListener("wheel", function (e) {
-//     if (e.deltaY > 0) item.scrollLeft += 100;
-//     else item.scrollLeft -= 100;
-// });
