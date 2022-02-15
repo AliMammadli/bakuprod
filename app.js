823c4f9de724dd5b323b7f48413fa9292d106567
Ali Mammadli <ali.mammadli@novum.tech>
2022-02-15 15:22:55 +0400

    Public folder created to serve

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const http = require('http')
 const express = require('express')
 const app = express()
 
-app.use(express.static(path.join(__dirname + 'index.html')))
-app.get('*', (req, res) => res.sendFile(path.join(__dirname + 'index.html')))
+app.use(express.static(path.join(__dirname + '/public')))
+app.get('*', (req, res) => res.sendFile(path.join(__dirname + '/public')))
+
+const port = process.env.PORT || 3000
 
 const server = http.createServer(app)
-server.listen(process.env.PORT, () => { console.log('Server Started') })
+server.listen(port, () => { console.log('Server Started') })
