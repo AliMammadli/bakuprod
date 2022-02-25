dbe1681cced14fad7daca04f6b6f1ca3de130502
Ali Mammadli <ali.mammadli@novum.tech>
2022-02-25 18:32:46 +0400

    Resources optimized

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path')
 
 module.exports = {
-    mode: 'development',
-    entry: './assets/scripts/three.js',
+    mode: 'production',
+    entry: ['./assets/scripts/three.js', './assets/scripts/slider.js'],
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'assets/scripts')
